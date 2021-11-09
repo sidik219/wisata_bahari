bf58feb4685273515ee593e0a0ce24404cd8be96
Sidik Mulyana <sidik.mlyn@gmail.com>
2021-11-09 19:40:01 +0700

    hak akses kelola kerjasama

--- models/view_kelola_kerjasama.php
+++ models/view_kelola_kerjasama.php
@@ -40,14 +40,14 @@ $rowKerjasama = $stmt->fetchAll();
     <div class="sidebar">
         <div class="sidebar-logo">
             <!-- Hak Akses Pengelola Wilayah atau Provinsi-->
-            <?php if ($level == 3 || $level == 4) { ?>
+            <?php if ($level == 2 || $level == 4) { ?>
             <h2><a href="view_dashboard_admin" style="color: #fff"><span class="fas fa-atom"></span>
             <span>Wisata Bahari</span></a></h2>
             <?php } ?>
         </div>
 
-        <!-- Hak Akses Pengelola Wilayah -->
-        <?php if ($level == 3) { ?>
+        <!-- Hak Akses Pengelola Lokasi -->
+        <?php if ($level == 2) { ?>
         <div class="sidebar-menu">
             <ul>
                 <!-- Dahboard Admin -->
@@ -55,6 +55,11 @@ $rowKerjasama = $stmt->fetchAll();
                     <a href="view_dashboard_admin">
                     <span class="icon fas fa-home"></span>
                         <span>Dashboard Admin</span></a>
+                </li>
+                <li>
+                    <a href="view_kelola_reservasi_wisata">
+                    <span class="fas fa-luggage-cart"></span>
+                        <span>Kelola Reservasi Wisata</span></a>
                 </li>
                 <li>
                     <a href="view_kelola_wisata">
@@ -67,19 +72,14 @@ $rowKerjasama = $stmt->fetchAll();
                         <span>Kelola Asuransi</span></a>
                 </li>
                 <li>
-                    <a href="view_kelola_lokasi">
-                    <span class="fas fa-map-marked-alt"></span>
-                        <span>Kelola Lokasi</span></a>
+                    <a href="view_kelola_kerjasama" class="paimon-active">
+                    <span class="fas fa-handshake"></span>
+                        <span>Kelola Kerjasama</span></a>
                 </li>
                 <li>
-                    <a href="view_kelola_wilayah">
-                    <span class="fas fa-place-of-worship"></span>
-                        <span>Kelola Wilayah</span></a>
-                </li>
-                <li>
-                    <a href="view_kelola_user">
-                    <span class="fas fa-users"></span>
-                        <span>Kelola User</span></a>
+                    <a href="view_kelola_pengadaan">
+                    <span class="fas fa-truck-loading"></span>
+                        <span>Kelola Pengadaan</span></a>
                 </li>
                 <li>
                     <a href="logout">
@@ -169,7 +169,7 @@ $rowKerjasama = $stmt->fetchAll();
             </div>-->
 
             <!-- Hak Akses Pengelola Wilayah atau Provinsi-->
-            <?php if ($level == 3 || $level == 4) { ?>
+            <?php if ($level == 2 || $level == 4) { ?>
             <div class="user-wrapper">
                 <img src="../views/img/paimon-5.png" width="50px" height="50px" alt="">
                 <div>
@@ -181,7 +181,7 @@ $rowKerjasama = $stmt->fetchAll();
         </header>
         
         <!-- Hak Akses Pengelola Wilayah atau Provinsi -->
-        <?php if ($level == 3 || $level == 4) { ?>
+        <?php if ($level == 2 || $level == 4) { ?>
         <!-- Main -->
         <main>
             <!-- Button Selanjutnya -->
